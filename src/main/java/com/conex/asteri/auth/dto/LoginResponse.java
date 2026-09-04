a167fc8157cfe33cc2e1335e0b0e183c11b8f0a6
chaufalover <tacuresancheztomas@gmail.com>
2026-09-04 16:28:43 -0500

    Comunicacion del backend con la API de Xui

--- src/main/java/com/conex/asteri/auth/dto/LoginResponse.java
+++ src/main/java/com/conex/asteri/auth/dto/LoginResponse.java
@@ -1,11 +1,15 @@
 package com.conex.asteri.auth.dto;
 
+import lombok.Builder;
+
 //Respondera a XuiAuthResponse
 //Necesario para poder manejar la data del JSON devuelto por el sv
 
+@Builder
 public record LoginResponse(
         String username,
         String status,
-        String experitionDate,
+        String expDate,
         String maxConnections) {
 }
+
--- src/main/java/com/conex/asteri/auth/service/AuthService.java
+++ src/main/java/com/conex/asteri/auth/service/AuthService.java
@@ -1,5 +1,39 @@
 package com.conex.asteri.auth.service;
 
-public class AuthService {
+import org.springframework.stereotype.Service;
 
+import com.conex.asteri.auth.dto.LoginRequest;
+import com.conex.asteri.auth.dto.LoginResponse;
+import com.conex.asteri.xui.client.XuiClient;
+import com.conex.asteri.xui.dto.XuiAuthResponse;
+
+import lombok.RequiredArgsConstructor;
+
+/*
+Logica de autenticacion
+1. Recibir peticion de login
+2.  Autentica las credenciales
+3.  Mapear la respuesta del sv a la respuesta deseada
+4. Devuelve la info del usuario
+*/
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final XuiClient xuiClient;
+
+    public LoginResponse login(LoginRequest request) {
+
+        XuiAuthResponse response = xuiClient.authenticate(
+                request.username(),
+                request.password());
+
+        return LoginResponse.builder()
+                .username(response.getUserInfo().username())
+                .status(response.getUserInfo().status())
+                .expDate(response.getUserInfo().expDate())
+                .maxConnections(response.getUserInfo().maxConnections())
+                .build();
+
+    }
 }
+
